c4d3659f6c425ccf939fe36094194f0a4252f828
mahadeook-art <mahadeook@gmail.com>
2026-04-11 17:51:02 +0700

    tambah struct

--- tubes_prak/Manajemen_tugas.cpp
+++ tubes_prak/Manajemen_tugas.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
+struct Tugas{
+    string nama;
+    int deadline;
+    int kesulitan;
+    int prioritas;
+    Tugas*next;
 
+};
 
 
 int main(){
-cout<<"heiworld";
+
 
 
 }
